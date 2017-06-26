c2603d7eb7855be6c3e512156809c3bd58d1bc81
Suan-Aik Yeo <suan@usertesting.com>
2017-06-26 16:13:10 -0700

    keep last 30 runs, instead of 5

--- app/jobs/purge_old_runs_job.rb
+++ app/jobs/purge_old_runs_job.rb
@@ -2,6 +2,6 @@ require 'image_processor'
 
 class PurgeOldRunsJob < ApplicationJob
   def perform(suite_id)
-    Suite.find(suite_id).runs.order(id: :desc).offset(5).destroy_all
+    Suite.find(suite_id).runs.order(id: :desc).offset(30).destroy_all
   end
 end
